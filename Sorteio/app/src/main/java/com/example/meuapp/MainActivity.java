0472626991e3d823d6bd0adcb2fac8049d280684
iranezequiel <iranezequiel36@gmail.com>
2026-07-03 15:19:58 -0300

    Sorteio

---
+++ Sorteio/app/src/main/java/com/example/meuapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.meuapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    int contador=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        TextView tv = findViewById(R.id.tv);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(v -> {
+          contador++;
+
+            Random random=new Random();
+            int r = random.nextInt(100);
+
+            tv.setText(Integer.toString(r));
+
+      });
+
+
+    }
+}
