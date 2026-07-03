eea65641d635de277af3454e3376ace9e778ab14
iranezequiel <iranezequiel36@gmail.com>
2026-07-03 15:18:35 -0300

    SlideShow

---
+++ SlideShow/app/src/main/java/com/example/meuapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.meuapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bntAvancar, bntVoltar;
+    ImageView imageView;
+
+    Integer imagens[] = new Integer[]{
+            R.drawable.cachorro,
+            R.drawable.gardem,
+            R.drawable.happy,
+            R.drawable.patinho,
+            R.drawable.porquinho
+    };
+
+    int posicao = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        bntAvancar=findViewById(R.id.buttonAvancar);
+        bntVoltar=findViewById(R.id.buttonVoltar);
+        imageView=findViewById(R.id.imageView);
+
+        bntAvancar.setOnClickListener(v -> {
+
+
+            imageView.setImageResource(imagens[posicao]);
+
+        });
+
+    }
+}
